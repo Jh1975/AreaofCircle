3b4ac3ae815dc59b1dcb5e7e14b2efb295a5904a
Jerome.Hart <Jh1on1@hotmail.com>
2021-10-24 23:26:23 -0400

    inital commit

---
+++ AreaofCircle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AreaofCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(“What is the radius of your circle ?”);
+            var radius = double.Parse(Console.ReadLine());
+
+            (Math.PI * radius * radius)
+        }
+    }
+}
